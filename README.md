e7347a5b3f253aca4e990045f39305363d9d5dcc
Sjors van Dongen <sitesjors@hotmail.com>
2017-08-28 09:09:59 +0200

    Import namespace improvements

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ A set of plugins for making (PHP) development easier in Sublime Text
 - Implement interface methods
 - HTML script:src and link:href autocompletions
 - Copy a specific line with ctrl+j
+- Select all class names in a view except those who already are imported
+
+# To do
+
+- Refactor ImportNamespaceCommand for multiple quick panel windows
+- Add New Trait option
+- Add New Interface option
--- sublime_basic/commands/ImportNamespaceCommand.py
+++ sublime_basic/commands/ImportNamespaceCommand.py
@@ -23,8 +23,18 @@ class ImportNamespaceCommand(sublime_plugin.TextCommand):
         # Find tags file
         tags_file = ctags.find_tags_relative_to(view.file_name(), ctags.setting("tag_file"))
 
+        # List definitions
+        not_found = []
+        symbols = []
+
+        # For each selection in the current view
         for sel in view.sel():
             symbol = view.substr(view.word(sel.begin()))
+
+            if symbol in symbols:
+                continue
+
+            symbols.append(symbol)
 
             # Find definition and filter the unnessecary keys
             results = ctags.JumpToDefinition.run(symbol, None, "", [], view, tags_file)
@@ -32,14 +42,19 @@ class ImportNamespaceCommand(sublime_plugin.TextCommand):
             # If results is more than one
             if results != None:
                 results = list(map(lambda x: [x.symbol, x.filename], results[0]))
-                if len(results) > 1:
+                if len(results) > 1 and len(view.sel()) == 1:
                     # Show selection panel
                     self.results = results
                     sublime.active_window().show_quick_panel(results, self.on_done)
+                elif len(results) > 1 and len(view.sel()) > 1:
+                    not_found.append(symbol)
                 else:
                     sublime.active_window().run_command("insert_namespace", { "item": results[0] })
             else:
                 sublime.status_message("No definitions found for " + symbol)
+
+        if len(not_found) > 0:
+            sublime.message_dialog("Could not import the following classes because of multiple occurences.\n\n - " + "\n - ".join(not_found))
 
     def on_done(self, index):
         """ ON NAMESPACE SELECTION """
